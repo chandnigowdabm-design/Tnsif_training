1746fd4d2c3f83a37faaffaa26002cb5de99efab
Chandini B M <Chandini B M@LAPTOP-L59AICEL>
2026-07-13 20:54:39 +0530

    First Commit

---
+++ TNSIF_JAVA/src/org/tnsif/ace/c2tc/java/helloworld.java
@@ -0,0 +1,11 @@
+package org.tnsif.ace.c2tc.java;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
