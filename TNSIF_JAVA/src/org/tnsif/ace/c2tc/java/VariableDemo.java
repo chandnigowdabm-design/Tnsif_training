5e5e10d70aa3bea03fa12b08e2b9ef0b537e17dd
Chandini B M <Chandini B M@LAPTOP-L59AICEL>
2026-07-16 10:54:15 +0530

    First Commit

---
+++ TNSIF_JAVA/src/org/tnsif/ace/c2tc/java/VariableDemo.java
@@ -0,0 +1,24 @@
+package org.tnsif.ace.c2tc.java;
+
+public class VariableDemo {
+	int price=10;
+	void print() {
+		String msg="chandini";
+		System.out.println(msg);
+	}
+static String message ="hello students";
+	
+
+	public static void main(String[] args) {
+		VariableDemo obj=new VariableDemo();
+		System.out.println(obj.price);
+		obj.print();
+		System.out.println(message);
+		// TODO Auto-generated method stub
+
+	}
+
+}
+		
+
+
--- TNSIF_JAVA/src/org/tnsif/ace/c2tc/java/helloworld.java
+++ TNSIF_JAVA/src/org/tnsif/ace/c2tc/java/helloworld.java
@@ -1,9 +1,11 @@
 package org.tnsif.ace.c2tc.java;
 
 public class helloworld {
+	
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello world");
+		
 		// TODO Auto-generated method stub
 
 	}
